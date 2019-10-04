f0ccffcccd314f6aa602f1c46dad60426876a637
Mothrakk <mothrakk1@gmail.com>
2019-10-04 17:03:57 +0300

    Added rudamentary input handling.

--- scriptlibs/ScriptManager.py
+++ scriptlibs/ScriptManager.py
@@ -1,9 +1,15 @@
+import subprocess
 import utility
 import scriptlibs.Script
 import scriptlibs.ScriptList
 
 class ScriptManager:
     def __init__(self) -> None:
+        self.HARDCOMMANDS = {
+            "exit":self.kill,
+            "help":self.help,
+            "cls":self.clear
+        }
         self.script_list = scriptlibs.ScriptList.ScriptList()
         self.scripts = [scriptlibs.Script.Script(p) for p in self.script_list.paths]
         for s in self.scripts:
@@ -14,14 +20,24 @@ class ScriptManager:
         return utility.file_read(utility.pipe_path(".autostart")).strip().split("\n")
 
     def handle_kb_input(self, inp: str) -> None:
-        if inp == "s":
-            self.scripts[0].start()
-        elif inp == "e":
-            self.scripts[0].kill()
-        elif inp == "poll":
-            utility.write_to_larva(utility.build_log(self.scripts[0].p.pid, "Larva"))
+        inp = inp.strip().split(" ")
+        if inp[0] in self.HARDCOMMANDS:
+            self.HARDCOMMANDS[inp[0]](inp)
 
     def handle_script_input(self) -> None:
         for line in utility.file_flush(utility.pipe_path("larva")).split("\n"):
             if line != "":
                 print(line)
+
+    def help(self, *argv: list) -> str:
+        utility.write_to_larva(utility.build_log("; ".join(self.HARDCOMMANDS.keys())))
+
+    def kill(self, *argv: list) -> None:
+        for s in self.scripts:
+            s.kill()
+        utility.write_to_larva(utility.build_log("Exiting"))
+        self.handle_script_input()
+        exit(0)
+
+    def clear(self, *argv: list) -> None:
+        subprocess.run("cls", shell=True)
